088a94e7014059b915c58172147b680bfe986443
gyu <zn7034@naver.com>
2026-02-06 16:11:20 +0900

    feat: build and push docker image to ecr

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🚀 배포 자동화 시작!"
-terraform apply -auto-approve
-go build -o server main.go
-echo "✅ 배포 완료! 서버를 실행합니다."
-./server
--- main.tf
+++ main.tf
@@ -3,5 +3,16 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-9988"
+  bucket        = "my-unique-bucket-name-9988"
+  force_destroy = true  # 이 줄을 추가해!
+}
+
+resource "aws_ecr_repository" "my_app" {
+  name = "my-go-app-repo"
+  force_delete = true
+}
+
+# 나중에 주소를 확인하기 위해 출력 설정
+output "ecr_repository_url" {
+  value = aws_ecr_repository.my_app.repository_url
 }
